29912c961ca3f915d696d29c3ca36951f377df0c
archko <archko@gmail.com>
2019-12-05 19:56:01 +0800

    fix channel

--- lib/common/bridge/network_channel.dart
+++ lib/common/bridge/network_channel.dart
@@ -4,27 +4,16 @@ import 'flutter_bridge.dart';
 import 'dart_channel.dart';
 
 class HttpChannel {
-  static Future<dynamic> post({Map args, DartToNativeCallback callback}) async {
+  static Future<dynamic> post({Map args}) async {
     args ?? HashMap();
     args['method'] = 'post';
-    FlutterMain.singleton.channel
-        .invokeMethod(_HTTP_CHANNEL, args)
-        .then((onValue) {
-      callback(onValue);
-    });
+    return FlutterMain.singleton.channel.invokeMethod(_HTTP_CHANNEL, args);
   }
 
-  static Future<dynamic> get({Map args, DartToNativeCallback callback}) async {
+  static Future<dynamic> get({Map args}) async {
     args ?? HashMap();
     args['method'] = 'get';
-    FlutterMain.singleton.channel
-        .invokeMethod(
-      _HTTP_CHANNEL,
-      args,
-    )
-        .then((onValue) {
-      callback(onValue);
-    });
+    return FlutterMain.singleton.channel.invokeMethod(_HTTP_CHANNEL, args);
   }
 
   static const _HTTP_CHANNEL = "http_channel";
