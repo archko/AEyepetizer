795209c06d1bb50f3effab896bc1d785bd08273a
archko <archko@gmail.com>
2020-02-08 09:42:11 +0800

    convert videolistpage to provider

--- lib/page/video/video_by_category_page.dart
+++ lib/page/video/video_by_category_page.dart
@@ -1,14 +1,12 @@
 import 'package:aeyepetizer/entity/acategory.dart';
-import 'package:aeyepetizer/entity/trending.dart';
-import 'package:aeyepetizer/entity/video_item.dart';
+import 'package:aeyepetizer/model/video_by_category_provider.dart';
 import 'package:aeyepetizer/model/video_by_category_view_model.dart';
 import 'package:aeyepetizer/page/video/video_detail_list_page.dart';
 import 'package:aeyepetizer/page/video/video_list_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_base/model/base_list_state.dart';
-import 'package:flutter_base/utils/string_utils.dart';
-import 'package:flutter_base/widget/list/pull_widget.dart';
+import 'package:flutter_base/model/provider_widget.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class VideoByCategoryPage extends StatefulWidget {
@@ -40,78 +38,44 @@ class _VideoByCategoryPageState extends State<VideoByCategoryPage>
   }
 
   @override
-  Future refresh() async {
-    viewModel.setPage(startPage);
-    await (viewModel as VideoByCategoryViewModel)
-        .loadData(viewModel.page, widget.category)
-        .then((trending) {
-      viewModel.setData(trending.itemList);
-      setState(() {
-        print("refresh end.${viewModel.page}, ${viewModel.getCount()}");
-        if (trending.itemList == null || trending.itemList.length < 1) {
-          refreshController.loadNoData();
-        } else {
-          refreshController.refreshCompleted(resetFooterState: true);
-        }
-      });
-    }).catchError((e) => setState(() {
-              print("refresh error,$e");
-              refreshController.loadFailed();
-            }));
-  }
-
-  @override
-  Future<void> loadMore() async {
-    if (viewModel.getCount() < 1) {
-      return refresh();
-    }
-    VideoByCategoryViewModel cViewModel = (viewModel as VideoByCategoryViewModel);
-    Trending trendingb = cViewModel.last;
-    if (trendingb == null || StringUtils.isEmpty(trendingb.nextPageUrl)) {
-      refreshController.loadNoData();
-      return null;
-    }
-    await cViewModel.loadMore(-1).then((trending) {
-      cViewModel.updateDataAndPage(trending.itemList, viewModel.page + 1);
-      setState(() {
-        if (trending.itemList == null || trending.itemList.length < 1) {
-          refreshController.loadNoData();
-        } else {
-          refreshController.refreshCompleted(resetFooterState: true);
-        }
-        print(
-            "loadMore end.${refreshController.footerStatus},${viewModel.page}, ${viewModel.getCount()}");
-      });
-    }).catchError((e) => setState(() {
-          print("loadMore error:$e");
-          refreshController.loadFailed();
-        }));
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.category.name),
       ),
-      body: PullWidget(
-        pullController: refreshController,
-        listCount: viewModel.getCount(),
-        itemBuilder: (BuildContext context, int index) =>
-            _renderItem(context, index),
-        header: MaterialClassicHeader(),
-        footer: ClassicFooter(
-          loadStyle: LoadStyle.HideAlways,
-        ),
-        onLoadMore: loadMore,
-        onRefresh: refresh,
+      body: ProviderWidget<VideoByCategoryProvider>(
+        model: VideoByCategoryProvider(
+            viewModel: viewModel,
+            refreshController: refreshController,
+            category: widget.category),
+        onModelInitial: (m) {
+          refreshController.requestRefresh();
+        },
+        builder: (context, model, childWidget) {
+          return Container(
+            margin: EdgeInsets.all(4),
+            child: SmartRefresher(
+              enablePullDown: true,
+              enablePullUp: true,
+              controller: refreshController,
+              onRefresh: model.refresh,
+              onLoading: model.loadMore,
+              header: MaterialClassicHeader(),
+              footer: ClassicFooter(),
+              child: ListView.builder(
+                itemCount: model.getCount(),
+                itemBuilder: (BuildContext context, int index) =>
+                    _renderItem(context, index, model.getVideos()[index]),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 
   //列表的ltem
-  _renderItem(context, index) {
-    var item = viewModel.data[index] as VideoItem;
+  _renderItem(context, index, item) {
     return GestureDetector(
       onTap: () {
         Navigator.of(context).push(
