89e70f9f58ba20b5836ea3e19ba9039480e6c037
archko <archko@gmail.com>
2020-02-09 19:17:40 +0800

    update deps

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -14,10 +14,10 @@ import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry;
 public final class GeneratedPluginRegistrant {
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
     ShimPluginRegistry shimPluginRegistry = new ShimPluginRegistry(flutterEngine);
-      cn.archko.flutter.base.flutter_base.FlutterBasePlugin.registerWith(shimPluginRegistry.registrarFor("cn.archko.flutter.base.flutter_base.FlutterBasePlugin"));
+    flutterEngine.getPlugins().add(new cn.archko.flutter.base.flutter_base.FlutterBasePlugin());
       top.kikt.ijkplayer.IjkplayerPlugin.registerWith(shimPluginRegistry.registrarFor("top.kikt.ijkplayer.IjkplayerPlugin"));
+      com.flutter_webview_plugin.FlutterWebviewPlugin.registerWith(shimPluginRegistry.registrarFor("com.flutter_webview_plugin.FlutterWebviewPlugin"));
     flutterEngine.getPlugins().add(new io.flutter.plugins.pathprovider.PathProviderPlugin());
       com.tekartik.sqflite.SqflitePlugin.registerWith(shimPluginRegistry.registrarFor("com.tekartik.sqflite.SqflitePlugin"));
-    flutterEngine.getPlugins().add(new io.flutter.plugins.webviewflutter.WebViewFlutterPlugin());
   }
 }
