fd3e6f2fd632166091d85d41e165339120f3423b
archko <archko@gmail.com>
2022-10-28 17:46:44 +0800

    添加api示例

--- lib/demo/CounterController.dart
+++ lib/demo/CounterController.dart
@@ -1,10 +1,14 @@
 import 'package:aeyepetizer/demo/CounterState.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_base/http/http_client.dart';
+import 'package:flutter_base/http/http_response.dart';
 import 'package:flutter_base/log/logger.dart';
-import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:http/http.dart';
 
 class CounterController extends GetxController {
   /// 定义了该变量为响应式变量，当该变量数值变化时，页面的刷新方法将自动刷新
-  var count = 0.obs;
+  var count = 0;
 
   /// 自增方法
   void increase() => ++count;
@@ -31,5 +35,27 @@ class CounterController extends GetxController {
     state.count++;
     count++;
     update();
+  }
+
+  Future<Map<String, dynamic>?> getToken() async {
+    Map<String, dynamic>? result;
+    try {
+      Map<String, dynamic> header = Map();
+      header['content-type'] = "application/x-www-form-urlencoded";
+      var url = "https://api.cnblogs.com/token";
+      
+      Map<String, dynamic> args = Map();
+      args['client_id'] = "1206278b-183a-4693-8cc9-acc2628306f6";
+      args['client_secret'] = "f-Nt_OnnFwJtywfgZ5_-JOr9O46VFZB8UfTWLgDfNi6kaB5hclaAGiRhg-M33nglndS_2wYRh3qAJC-s";
+      args['grant_type'] = "client_credentials";
+
+      HttpResponse httpResponse =
+          await HttpClient.instance.post(url, header: header, data: args);
+      String string = httpResponse.data as String;
+      Logger.d("result:$string");
+    } catch (e) {
+      print(e);
+    }
+    return result;
   }
 }
